890e0215a3f1327cd458bb2e1f4720827fbaa773
unknown <carlosvargas.es@hotmail.com>
2022-06-24 14:19:55 -0500

    Desarrolloprefinalcards

--- frontend/src/pages/AllPlanets.jsx
+++ frontend/src/pages/AllPlanets.jsx
@@ -7,13 +7,13 @@ const AllPlanets = () => {
             <h1>All Planets</h1>
             <ToggleSearchButton/>
             <div className='wrapper'>
-              <Card title="Mercurio" distance=" 54 millones" imagen="../assets/images\planets/Mercurio.png"/>
-              <Card title="Venus" distance=" 54 millones"/>
-              <Card title="Marte" distance=" 54 millones"/>
-              <Card title="Jupiter"distance=" 54 millones"/>
-              <Card title="Saturno" distance=" 54 millones"/>
-              <Card title="Urano" distance=" 54 millones"/>
-              <Card title="Neptuno" distance=" 54 millones"/>
+              <Card title="Mercurio" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1Apl3uFSp49AU305oD6J86PS6G5uJFtVk"/>
+              <Card title="Venus" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1ISr5JmuHntNv9-x8yeH-SXAHahx-RLwF"/>
+              <Card title="Marte" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1Zlu3gCPBHnRCoqKME6BfUQanPDAijrAh"/>
+              <Card title="Jupiter"distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1WzzUDMNab1vvcw4wfU2Ryy9hSMaarp-h"/>
+              <Card title="Saturno" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1wEuo0Yw-S-QJ-VFwWSeCmsDjQqfU7lg0"/>
+              <Card title="Urano" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1H-3GuCQ7NSrkq5mq59OSzh7WFit2PwwQ"/>
+              <Card title="Neptuno" distance=" 54 millones" imagen="https://drive.google.com/uc?export=view&id=1AvqXEFMT751FGbjyP2KDueEX1ES0MZIG"/>
       </div>
         </div>
     );
