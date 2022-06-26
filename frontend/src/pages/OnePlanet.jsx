6d696f7f5999633bf31ea76e5fbfbfa12f36d668
Ruben35 <ruben111416@gmail.com>
2022-06-25 23:32:09 -0500

    Added instructions for consuming API on OnePlanet

--- frontend/src/pages/OnePlanet.jsx
+++ frontend/src/pages/OnePlanet.jsx
@@ -1,19 +1,40 @@
-import { useEffect, Suspense } from "react";
+import { useEffect, Suspense, useState } from "react";
 import { useParams } from "react-router";
 import { useNavigate } from "react-router-dom";
 import { BackButton, Planet3DModel } from "../components";
+import axios from 'axios'
 
 const OnePlanet = () => {
 
-    const { planet } = useParams();
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
     const availablePlantes = ['Mercurio', 'Venus', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'];
+    
+    const { planet } = useParams(); //Param from URL
+    
+    //States
+    const [ percentage, setPercentage ] = useState(90.23);
+    const [ distance, setDistance ] = useState("54 millones (km)");
+    const [ time, setTime ] = useState("300 años");
+    const [ moreInfo, setMoreInfo ] = useState("Este planeta no cuenta con un campo magnético que le proteja de la radiación y los vientos solares.");
 
     useEffect(() => {
         if (!availablePlantes.includes(planet)) {
             navigate("/404")
         } else {
+
             console.log(planet);
+
+            // TODO: Aquí realizar peticion a servidor con nombre de planeta ( variable "planet" )
+            axios.get('http://localhost:5000/flask/hello').then(response => {
+                console.log("SUCCESS", response)
+                // TODO: Aquí llenar los estados:
+                //!  - percentage con setPercentage
+                //!  - distance con setDistance
+                //!  - time con setTime
+                //!  - moreInfo con setMoreInfo
+            }).catch(error => {
+                console.log("ERROR",error)
+            })
         }
     }, []);
 
@@ -32,16 +53,16 @@ const OnePlanet = () => {
                         Similitud Exoplaneta Ideal:
                     </p>
                     <p className="percentage">
-                        90.23%
+                        {percentage}%
                     </p>
                     <p>
-                        <b> Distancia: </b> 54 millones (km)
+                        <b> Distancia: </b> {distance}
                     </p>
                     <p>
-                        <b> Tiempo:  </b> 300 años
+                        <b> Tiempo:  </b> {time}
                     </p>
                     <p className="more-info">
-                        Este planeta no cuenta con un campo magnético que le proteja de la radiación y los vientos solares.
+                        {moreInfo}
                     </p>
                 </div>
             </div>
